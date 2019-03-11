c92238ae12a05c8fcb19e73a866a3f6c7a325dbc
root <root@ip-172-31-38-210.ec2.internal>
2019-03-11 07:03:33 +0000

    This is my first commit with git-maven

---
+++ src/main/java/lokesh/org/com/App.java
@@ -0,0 +1,13 @@
+package lokesh.org.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
